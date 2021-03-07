6ac4a4948f7e05f7e0133629b05100d9ca60a371
pradeep bhati <pradeep9692@gmail.com>
2021-03-07 17:18:08 +0530

    testing done

--- src/main/java/com/bitwelkin/businesslogicsever/controller/TestController.java
+++ src/main/java/com/bitwelkin/businesslogicsever/controller/TestController.java
@@ -1,5 +1,13 @@
 package com.bitwelkin.businesslogicsever.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
 
+	@GetMapping("/test")
+	public String testMethod() {
+		return "test";
+	}
 }
